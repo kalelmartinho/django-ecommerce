9b1cb6b4d58ede00e742005c9147d89c2a62bb1b
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 17:33:59 -0300

    refactor: Change Brand, Category to be unique

--- src/django_ecommerce/product/models.py
+++ src/django_ecommerce/product/models.py
@@ -3,7 +3,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
@@ -15,7 +15,7 @@ class Category(MPTTModel):
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):  # type: ignore
         return self.name
