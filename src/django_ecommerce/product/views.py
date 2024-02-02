dff36807438b8123097a599ce8abf6ac7d760819
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 00:49:05 -0300

    feat: Add viewset for Brands, change modelviewset to ViewSet

--- src/django_ecommerce/product/views.py
+++ src/django_ecommerce/product/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
-
-from .models import Category
-from .serializers import CategorySerializer
+from drf_spectacular.utils import extend_schema
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+from .models import Brand, Category
+from .serializers import BrandSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
     """
-    Simple ViewSet for viewing and editing categories.
+    ViewSet for viewing and editing categories.
     """
 
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
     
+    @extend_schema(responses=CategorySerializer)
     def list(self, request: Request, *args, **kwargs) -> Response:
         queryset = Category.objects.all()
         serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    ViewSet for viewing and editing brands.
+    """
+
+    queryset = Brand.objects.all()
+    
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        queryset = Brand.objects.all()
+        serializer = BrandSerializer(queryset, many=True)
         return Response(serializer.data)
