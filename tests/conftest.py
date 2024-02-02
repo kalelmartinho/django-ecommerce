8dec6dbb2de1edb0ba1f300949d08d8c6a265f23
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 13:14:04 -0300

    test: Add test for str dunder method of product

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pytest_factoryboy import register
 
-from tests.factories import BrandFactory, CategoryFactory
+from tests.factories import BrandFactory, CategoryFactory, ProductFactory
 
 register(BrandFactory)
 register(CategoryFactory)
+register(ProductFactory)
