6723b4a317b59e4941c40d9019b7d8fd46317c21
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 00:28:25 -0300

    feat: Add router for swagger docs with spectacular

--- src/django_ecommerce/urls.py
+++ src/django_ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from django_ecommerce.product import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r"category", views.CategoryViewSet)
@@ -10,4 +11,6 @@ router.register(r"category", views.CategoryViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
 ]
