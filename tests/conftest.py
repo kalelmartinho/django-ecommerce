6e448fc7ab3ade4f0314f81f486e11dd937d66ce
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 17:33:33 -0300

    test: Add fixture for APIClient to e2e

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 from pytest_factoryboy import register
-
+from rest_framework.test import APIClient
 from tests.factories import BrandFactory, CategoryFactory, ProductFactory
+import pytest
 
 register(BrandFactory)
 register(CategoryFactory)
 register(ProductFactory)
+
+pytest.fixture
+
+
+def client():
+    return APIClient()
