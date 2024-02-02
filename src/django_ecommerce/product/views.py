93ca73392b98750f2a06af36551b60960ad64b80
Kalel L. Martinho <kalel@tutamail.com>
2024-02-01 23:40:28 -0300

    feat: Add ViewSet and list method for Category

--- src/django_ecommerce/product/views.py
+++ src/django_ecommerce/product/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    Simple ViewSet for viewing and editing categories.
+    """
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
