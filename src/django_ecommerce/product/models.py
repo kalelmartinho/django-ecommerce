0c2dad02d12ef6f540e5721d643828f5dbc74798
Kalel L. Martinho <kalel@tutamail.com>
2024-02-02 21:05:33 -0300

    feat: Add model for ProductLine

--- src/django_ecommerce/product/models.py
+++ src/django_ecommerce/product/models.py
@@ -28,6 +28,18 @@ class Product(models.Model):
     category = TreeForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True
     )
+    is_available = models.BooleanField(default=False)
 
     def __str__(self):  # type: ignore
         return self.name
+
+
+class ProductLine(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=100, unique=True)
+    is_available = models.BooleanField(default=False)
+
+    def __str__(self):  # type: ignore
+        return f"{self.product.name} - {self.quantity} - {self.price}"
