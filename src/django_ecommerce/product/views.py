adc85f7207cbe781787c802ea986600c47508ed3
Kalel L. Martinho <kalel@tutamail.com>
2024-02-03 11:23:28 -0300

    feat: Add view to retrieve Product by category

--- src/django_ecommerce/product/views.py
+++ src/django_ecommerce/product/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
 from drf_spectacular.utils import extend_schema
-
+from rest_framework.decorators import action
 
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
@@ -15,7 +15,7 @@ class CategoryViewSet(viewsets.ViewSet):
     """
 
     queryset = Category.objects.all()
-    
+
     @extend_schema(responses=CategorySerializer)
     def list(self, request: Request, *args, **kwargs) -> Response:
         serializer = CategorySerializer(self.queryset, many=True)
@@ -27,21 +27,39 @@ class BrandViewSet(viewsets.ViewSet):
     ViewSet for viewing and editing brands.
     """
 
-    queryset = Brand.objects.all()
-    
+    queryset = Brand.objects.all()  # type: ignore
+
     @extend_schema(responses=BrandSerializer)
     def list(self, request: Request, *args, **kwargs) -> Response:
         serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
+
 
 class ProductViewSet(viewsets.ViewSet):
     """
     ViewSet for viewing and editing products.
     """
 
-    queryset = Product.objects.all()
-    
+    queryset = Product.objects.all()  # type: ignore
+
     @extend_schema(responses=ProductSerializer)
     def list(self, request: Request, *args, **kwargs) -> Response:
+        """
+        List all products.
+        """
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    @extend_schema(responses=ProductSerializer)
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="category/(?P<category>[^/.]+)",
+    )
+    def list_by_category_name(self, request: Request, category: str) -> Response:
+        """
+        List products by category name.
+        """
+        queryset = self.queryset.filter(category__name=category)
+        serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
